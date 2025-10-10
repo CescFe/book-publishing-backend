3bb64353da3ade9c5b9298f9c927c2408cb05d09
FrancescFe <fraferru@gmail.com>
2025-10-10 20:29:51 +0200

    Feat (BE-007): add AuthorEntity.kt

---
+++ src/main/kotlin/org/cescfe/bookpublishing/author/infrastructure/adapters/output/persistence/AuthorEntity.kt
@@ -0,0 +1,55 @@
+package org.cescfe.bookpublishing.author.infrastructure.adapters.output.persistence
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.PrePersist
+import jakarta.persistence.PreUpdate
+import jakarta.persistence.Table
+import jakarta.persistence.Version
+import java.sql.Timestamp
+import java.time.Instant
+import java.util.UUID
+
+@Entity
+@Table(name = "person", schema = "publishing")
+data class AuthorEntity(
+    @Id
+    @Column(name = "id", columnDefinition = "UUID")
+    val id: UUID,
+
+    @Version
+    @Column(name = "version")
+    val version: Long = 1,
+
+    @Column(name = "full_name", nullable = false)
+    val fullName: String,
+
+    @Column(name = "pseudonym")
+    val pseudonym: String?,
+
+    @Column(name = "biography", columnDefinition = "TEXT")
+    val biography: String?,
+
+    @Column(name = "email")
+    val email: String?,
+
+    @Column(name = "website")
+    val website: String?,
+) {
+    @Column(name = "created_at", nullable = false, updatable = false)
+    lateinit var createdAt: Timestamp
+
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    lateinit var updatedAt: Timestamp
+
+    @PrePersist
+    fun prePersist() {
+        this.createdAt = Timestamp.from(Instant.now())
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        this.updatedAt = Timestamp.from(Instant.now())
+    }
+}
