3b76b684628feb54a8b7c4f97bc361e4cc2cd8b8
FrancescFe <fraferru@gmail.com>
2025-11-17 12:28:03 +0100

    Test (BE-024): improve author domain model test coverage

--- src/test/kotlin/org/cescfe/bookpublishing/author/domain/model/AuthorTest.kt
+++ src/test/kotlin/org/cescfe/bookpublishing/author/domain/model/AuthorTest.kt
@@ -3,6 +3,7 @@ package org.cescfe.bookpublishing.author.domain.model
 import org.cescfe.bookpublishing.author.domain.exception.AuthorDomainException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.util.UUID
 import kotlin.test.assertEquals
 
 class AuthorTest {
@@ -68,6 +69,32 @@ class FullNameTest {
                 FullName(longName)
             }
         assertEquals("Full name must be between 1 and 255 characters", exception.message)
+    }
+}
+
+class AuthorIdTest {
+    @Test
+    fun `should generate unique AuthorId`() {
+        // When
+        val id1 = AuthorId.generate()
+        val id2 = AuthorId.generate()
+
+        // Then
+        assertEquals(UUID::class.java, id1.value.javaClass)
+        assertEquals(UUID::class.java, id2.value.javaClass)
+        assert(id1 != id2)
+    }
+
+    @Test
+    fun `should create AuthorId from string`() {
+        // Given
+        val uuidString = "da420b0a-64aa-470d-991e-7fcb7a936229"
+
+        // When
+        val authorId = AuthorId.fromString(uuidString)
+
+        // Then
+        assertEquals(uuidString, authorId.value.toString())
     }
 }
 
