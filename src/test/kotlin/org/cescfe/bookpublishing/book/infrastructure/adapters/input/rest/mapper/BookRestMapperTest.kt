b35f43d97a579061f8f2be25813d0ff34a5dd4e1
FrancescFe <fraferru@gmail.com>
2025-11-24 20:47:42 +0100

    Feat (BE-041): add create book controller tests

---
+++ src/test/kotlin/org/cescfe/bookpublishing/book/infrastructure/adapters/input/rest/mapper/BookRestMapperTest.kt
@@ -0,0 +1,67 @@
+package org.cescfe.bookpublishing.book.infrastructure.adapters.input.rest.mapper
+
+import org.cescfe.bookpublishing.book.objectMothers.BookObjectMother
+import org.junit.jupiter.api.Test
+import java.net.URI
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class BookRestMapperTest {
+    private val mapper = BookRestMapper()
+
+    @Test
+    fun `should map domain book to response DTO`() {
+        // Given
+        val book = BookObjectMother.createWithAllFields()
+
+        // When
+        val dto = mapper.toDto(book)
+
+        // Then
+        assertEquals(book.id.value, dto.id)
+        assertEquals(book.title.value, dto.title)
+        assertEquals(book.authorId.value, dto.authorId)
+        assertEquals(book.collectionId.value, dto.collectionId)
+        assertEquals(book.readingLevel!!.name, dto.readingLevel!!.name)
+        assertEquals(book.primaryLanguage!!.name, dto.primaryLanguage!!.name)
+        assertEquals(book.secondaryLanguages!!.value.map { it.name }, dto.secondaryLanguages!!.map { it.name })
+        assertEquals(book.primaryGenre!!.name, dto.primaryGenre!!.name)
+        assertEquals(book.secondaryGenres!!.value.map { it.name }, dto.secondaryGenres!!.map { it.name })
+        assertEquals(book.basePrice.value, dto.basePrice)
+        assertEquals(book.vatRate!!.value, dto.vatRate!!)
+        assertEquals(book.isbn!!.value, dto.isbn)
+        assertEquals(book.publicationDate!!.value, dto.publicationDate)
+        assertEquals(book.pageCount!!.value, dto.pageCount)
+        assertEquals(URI.create(book.coverImagePath!!.value), dto.coverImageUrl)
+        assertEquals(book.description!!.value, dto.description)
+    }
+
+    @Test
+    fun `should map minimal domain book to response DTO`() {
+        // Given
+        val book = BookObjectMother.createMinimal()
+
+        // When
+        val dto = mapper.toDto(book)
+
+        // Then
+        assertNotNull(dto)
+        assertEquals(book.id.value, dto.id)
+        assertEquals(book.authorId.value, dto.authorId)
+        assertEquals(book.collectionId.value, dto.collectionId)
+        assertEquals(book.title.value, dto.title)
+        assertEquals(book.basePrice.value, dto.basePrice)
+        assertNull(dto.readingLevel)
+        assertNull(dto.primaryLanguage)
+        assertNull(dto.secondaryLanguages)
+        assertNull(dto.primaryGenre)
+        assertNull(dto.secondaryGenres)
+        assertNull(dto.vatRate)
+        assertNull(dto.isbn)
+        assertNull(dto.publicationDate)
+        assertNull(dto.pageCount)
+        assertNull(dto.coverImageUrl)
+        assertNull(dto.description)
+    }
+}
