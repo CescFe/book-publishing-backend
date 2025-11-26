38e741bbbb1cbf8f5383a5e3123d2f3d1eae674e
FrancescFe <fraferru@gmail.com>
2025-11-26 23:02:33 +0100

    Feat (BE-056): fix SecurityConfigIT.kt

--- src/test/kotlin/org/cescfe/bookpublishing/shared/infrastructure/adapters/input/config/SecurityConfigIT.kt
+++ src/test/kotlin/org/cescfe/bookpublishing/shared/infrastructure/adapters/input/config/SecurityConfigIT.kt
@@ -4,19 +4,36 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
+@ActiveProfiles("test")
 class SecurityConfigIT {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     companion object {
         private const val AUTHORS_URI = "/api/v1/authors"
+
+        @Container
+        @ServiceConnection
+        @JvmStatic
+        val postgres: PostgreSQLContainer<*> =
+            PostgreSQLContainer(DockerImageName.parse("postgres:16"))
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test")
     }
 
     @Test
