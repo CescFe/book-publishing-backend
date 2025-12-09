34a9363f892e53b9bba27633f702735cd72a2ecd
FrancescFe <fraferru@gmail.com>
2025-12-09 19:01:44 +0100

    Feat (BE-063): apply spotless

--- src/main/kotlin/org/cescfe/bookpublishing/shared/infrastructure/adapters/input/config/SecurityConfig.kt
+++ src/main/kotlin/org/cescfe/bookpublishing/shared/infrastructure/adapters/input/config/SecurityConfig.kt
@@ -1,20 +1,19 @@
 package org.cescfe.bookpublishing.shared.infrastructure.adapters.input.config
 
+import org.cescfe.bookpublishing.auth.domain.service.UserService
 import org.cescfe.bookpublishing.shared.infrastructure.adapters.input.security.JwtRequestFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-
-import org.cescfe.bookpublishing.auth.domain.service.UserService
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
