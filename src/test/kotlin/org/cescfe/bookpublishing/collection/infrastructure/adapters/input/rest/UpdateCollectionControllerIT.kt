e100f5e33863086f4c4fb447c3f2763b986c784c
FrancescFe <fraferru@gmail.com>
2026-01-21 00:56:22 +0100

    refactor (075): refactor integration test

--- src/test/kotlin/org/cescfe/bookpublishing/collection/infrastructure/adapters/input/rest/UpdateCollectionControllerIT.kt
+++ src/test/kotlin/org/cescfe/bookpublishing/collection/infrastructure/adapters/input/rest/UpdateCollectionControllerIT.kt
@@ -1,16 +1,21 @@
-package org.cescfe.bookpublishing.collection.infrastructure.adapters
+package org.cescfe.bookpublishing.collection.infrastructure.adapters.input.rest
 
 import org.cescfe.bookpublishing.collection.application.port.input.UpdateCollectionUseCase
 import org.cescfe.bookpublishing.collection.objectMothers.CollectionObjectMother
+import org.cescfe.bookpublishing.shared.domain.model.enum.Genre
+import org.cescfe.bookpublishing.shared.domain.model.enum.Language
+import org.cescfe.bookpublishing.shared.domain.model.enum.ReadingLevel
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
@@ -18,6 +23,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.util.UUID
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
@@ -35,12 +41,6 @@ class UpdateCollectionControllerIT {
 
     @MockitoBean
     private lateinit var updateCollectionUseCase: UpdateCollectionUseCase
-
-    companion object {
-        private const val URI = "/api/v1/collections/%s"
-        private const val ROLE = "ROLE_ADMIN"
-        private const val TEST_COLLECTION_ID = "223e4567-e89b-12d3-a456-426614174000"
-    }
 
     @BeforeEach
     fun setup() {
@@ -62,6 +62,7 @@ class UpdateCollectionControllerIT {
 
     @Test
     fun `should update collection successfully`() {
+        // Given
         val requestBody =
             """
             {
@@ -74,14 +75,17 @@ class UpdateCollectionControllerIT {
             }
             """.trimIndent()
 
+        // When
         mockMvc
             .perform(
                 MockMvcRequestBuilders
                     .put(URI.format(TEST_COLLECTION_ID))
-                    .with(jwt().authorities(SimpleGrantedAuthority(ROLE)))
+                    .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(SimpleGrantedAuthority(ROLE)))
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON),
-            ).andExpect(MockMvcResultMatchers.status().isOk)
+            )
+            // Then
+            .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(TEST_COLLECTION_ID))
             .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Updated Collection Name"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.reading_level").value("ADULT"))
@@ -91,5 +95,22 @@ class UpdateCollectionControllerIT {
             .andExpect(MockMvcResultMatchers.jsonPath("$.primary_genre").value("FANTASY"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.secondary_genres[0]").value("ADVENTURE"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.secondary_genres[1]").doesNotExist())
+
+        val commandCaptor = argumentCaptor<UpdateCollectionUseCase.Command>()
+        verify(updateCollectionUseCase).execute(any(), commandCaptor.capture())
+
+        val capturedCommand = commandCaptor.firstValue
+        assertEquals("Updated Collection Name", capturedCommand.name)
+        assertEquals(ReadingLevel.ADULT, capturedCommand.readingLevel)
+        assertEquals(Language.ENGLISH, capturedCommand.primaryLanguage)
+        assertEquals(listOf(Language.CATALAN, Language.SPANISH), capturedCommand.secondaryLanguages)
+        assertEquals(Genre.FANTASY, capturedCommand.primaryGenre)
+        assertEquals(listOf(Genre.ADVENTURE), capturedCommand.secondaryGenres)
+    }
+
+    companion object {
+        private const val URI = "/api/v1/collections/%s"
+        private const val ROLE = "ROLE_ADMIN"
+        private const val TEST_COLLECTION_ID = "223e4567-e89b-12d3-a456-426614174000"
     }
 }
