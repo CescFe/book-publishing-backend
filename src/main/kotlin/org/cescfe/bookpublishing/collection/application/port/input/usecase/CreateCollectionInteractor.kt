fbe0f4368dc6589688a4400b378a65a251867e91
FrancescFe <fraferru@gmail.com>
2026-01-20 22:20:41 +0100

    feat (074): create collection ensures name uniqueness

--- src/main/kotlin/org/cescfe/bookpublishing/collection/application/port/input/usecase/CreateCollectionInteractor.kt
+++ src/main/kotlin/org/cescfe/bookpublishing/collection/application/port/input/usecase/CreateCollectionInteractor.kt
@@ -4,6 +4,7 @@ import org.cescfe.bookpublishing.collection.application.port.input.CreateCollect
 import org.cescfe.bookpublishing.collection.application.port.input.mapper.CreateCollectionUseCaseMapper
 import org.cescfe.bookpublishing.collection.domain.model.Collection
 import org.cescfe.bookpublishing.collection.domain.port.CollectionRepository
+import org.cescfe.bookpublishing.collection.domain.service.CollectionDomainService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,8 +13,10 @@ import org.springframework.transaction.annotation.Transactional
 class CreateCollectionInteractor(
     private val collectionRepository: CollectionRepository,
     private val mapper: CreateCollectionUseCaseMapper,
+    private val collectionDomainService: CollectionDomainService,
 ) : CreateCollectionUseCase {
     override fun execute(command: CreateCollectionUseCase.Command): Collection {
+        collectionDomainService.ensureNameUniqueness(command.name)
         val collection = mapper.toDomain(command)
         return collectionRepository.save(collection)
     }
